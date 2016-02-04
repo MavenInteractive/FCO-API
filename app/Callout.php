b5576e6567b26febe725734243f04ec1aaef0f9b
Jofil Inoc Baring <jofil.baring@gmail.com>
2016-02-03 17:33:17 -0700

    Update callouts.

--- app/Callout.php
+++ app/Callout.php
@@ -19,6 +19,33 @@ class Callout extends Model {
     protected $fillable = ['user_id', 'category_id', 'title', 'description', 'fighter_a', 'fighter_b', 'photo', 'video', 'match_type', 'details_date', 'details_time', 'details_venue', 'total_comments', 'total_views', 'total_votes', 'status', 'latitude', 'longitude', 'broadcast_url', 'ticket_url'];
 
     /**
+     * The attributes that are custom.
+     *
+     * @var array
+     */
+    protected $appends = ['down', 'up'];
+
+    /**
+     * The # of down votes
+     *
+     * @return integer
+     */
+    public function getDownAttribute()
+    {
+        return $this->vote()->where('tally', '<', 0)->count();
+    }
+
+    /**
+     * The # of up votes
+     *
+     * @return integer
+     */
+    public function getUpAttribute()
+    {
+        return $this->vote()->where('tally', '>', 0)->count();
+    }
+
+    /**
      * The users relationship.
      *
      * @return object
