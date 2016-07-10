7a85a6e68e79af09a1cae17c492ddadad49bdbfa
Jofil Inoc Baring <jofil.baring@gmail.com>
2016-07-10 12:48:22 +0800

    Change thank you message.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 			<div class="panel panel-default">
 				<div class="panel-heading">Fight Call Out</div>
 				<div class="panel-body">
-					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+					You have successfully changed your password, you can now go back to the app and login, thank you
 				</div>
 			</div>
 		</div>
